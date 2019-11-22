559c6b28d8deef07b7e2622df469684543926bba
Sean Blessing <snblessing@gmail.com>
2019-11-22 11:48:34 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular-world-jbc201909';
+  title:string = 'Happy Hour Friday is here!!!';
 }
